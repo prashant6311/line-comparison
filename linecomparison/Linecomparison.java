892b445adbc31a57e69adf6d69a6f61536f10fd6
prashant6311 <prashantppatil761@gmail.com>
2022-06-28 07:40:59 +0530

    ADDED: Welcome Line Comparison Computation Progarm

---
+++ linecomparison/Linecomparison.java
@@ -0,0 +1,9 @@
+package bridgelabz.linecomparison;
+
+public class Linecomparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparison Computation Progarm");
+	}
+
+}
